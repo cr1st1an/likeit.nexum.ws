cab1e4d65f122e7a68fbbf6b2c47cbde0a9d326e
cr1st1an <inbox@cris.mx>
2013-02-23 21:04:41 -0800

    [Checkpoint] Zoom & Info

--- api/lib/data_cleaner.php
+++ api/lib/data_cleaner.php
@@ -7,11 +7,33 @@ class DataCleaner {
 
         foreach ($FEED_DATA as $key => $data) {
             $photos_data[$key]['id'] = $data['id'];
-            $photos_data[$key]['src_150'] = $data['images']['thumbnail']['url'];
-            $photos_data[$key]['src_306'] = $data['images']['low_resolution']['url'];
-            $photos_data[$key]['src_612'] = $data['images']['standard_resolution']['url'];
+            $photos_data[$key]['caption'] = $data['caption']['text'];
+            
+            $photos_data[$key]['author'] = array();
+            $photos_data[$key]['author']['id'] = $data['user']['id'];
+            $photos_data[$key]['author']['username'] = $data['user']['username'];
+            $photos_data[$key]['author']['profile_picture'] = $data['user']['profile_picture'];
+            
+            $photos_data[$key]['location'] = array();
+            $photos_data[$key]['location']['id'] = $data['location']['id'];
+            $photos_data[$key]['location']['name'] = $data['location']['name'];
+            
+            $photos_data[$key]['urls'] = array();
+            $photos_data[$key]['urls']['150'] = $data['images']['thumbnail']['url'];
+            $photos_data[$key]['urls']['306'] = $data['images']['low_resolution']['url'];
+            $photos_data[$key]['urls']['612'] = $data['images']['standard_resolution']['url'];
+            
+            $photos_data[$key]['tags'] = $data['tags'];
+            
+            $photos_data[$key]['likes'] = array();
+            $photos_data[$key]['likes']['count'] = $data['likes']['count'];
+            foreach($data['likes']['data'] as $key_like => $data_like){
+                $photos_data[$key]['likes'][$key_like]['id'] = $data_like['id'];
+                $photos_data[$key]['likes'][$key_like]['profile_picture'] = $data_like['profile_picture'];
+            }
+            
         }
-
+        
         return $photos_data;
     }
 
